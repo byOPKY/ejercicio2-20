c9f64ca3aa740d30e17937c57b5bb0149c90e954
byOPKY <orrillokarol@gmail.com>
2016-09-06 18:45:45 -0500

    subiendo los ejercicios

---
+++ main.js
@@ -0,0 +1,22 @@
+var feature ="closures";
+(function () {
+  if (typeof feature ==="undefined")
+  {
+  	console.log("JS coders love its" + feature);
+  }
+  else{
+  	console.log("JS developers love its" + feature);
+  }
+})();
+
+/*var feature ="closures";
+(function () {
+  if (typeof feature ==="undefined")
+  {
+  	var feature ="callbacks";
+  	console.log("JS coders love its" + feature);
+  }
+  else{
+  	console.log("JS developers love its" + feature);
+  }
+})();*/
